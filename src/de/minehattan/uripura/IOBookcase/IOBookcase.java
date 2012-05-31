de09217bba1e5cf077afafa1cf49d76d5c714d66
TheE <thee@online.de>
2012-05-31 11:57:56 +0200

    Added reload command, updated plugin.yml and moved some things around.

--- src/de/minehattan/uripura/IOBookcase/IOBookcase.java
+++ src/de/minehattan/uripura/IOBookcase/IOBookcase.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -44,18 +48,35 @@ public class IOBookcase extends JavaPlugin {
 		saveConfig();
 		loadConfig();
 
-		if (!checkFile())
-			createFile();
-		if (!checkDatabase())
-			createDatabase();
-		if (!checkBookFile())
-			createBookFile();
+		createFile();
+		createDatabase();
+		createBookFile();
 
 		this.logMessage("Enabeld");
 	}
 
 	public void onDisable() {
 		this.logMessage("Disabeld");
+	}
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("iobookcase")) {
+			if (sender instanceof Player
+					&& !((Player) sender).hasPermission("iobookcase.reload"))
+				return true;
+
+			if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+				reloadConfig();
+				loadConfig();
+				sender.sendMessage(ChatColor.YELLOW
+						+ "IOBookcase configuration file reloaded.");
+			} else
+				sender.sendMessage(ChatColor.YELLOW
+						+ "Use '/iobookcase reload' to reload the configuration file.");
+		}
+
+		return true;
 	}
 
 	public void logMessage(String msg) {
@@ -107,54 +128,64 @@ public class IOBookcase extends JavaPlugin {
 	}
 	
 	private void createDatabase() {
-		this.logMessage("bookcase.db does not exist. Creating...");
-		IOBookcaseDatabase database = new IOBookcaseDatabase();
-		if (database.createDatabase()) {
-			this.logMessage("Database created!");
-		} else
-			this.logMessage("Cannot create Database!");
+		if (!checkDatabase()) {
+			this.logMessage("bookcase.db does not exist. Creating...");
+			IOBookcaseDatabase database = new IOBookcaseDatabase();
+			if (database.createDatabase()) {
+				this.logMessage("Database created!");
+			} else
+				this.logMessage("Cannot create Database!");
+		}
 	}
 
 	private void createFile() {
-		File importFile = new File(getDataFolder() + File.separator + "import.xml");
-		new File(getDataFolder().toString()).mkdir();
-		this.logMessage("import.xml does not exist. Creating...");
-		try {
-			importFile.createNewFile();
-			this.logMessage("File created!");
-		} catch (IOException e) {
-			System.out.println("Cannot create File " + importFile.getPath() + File.separator + "import.xml");
+		if (!checkFile()) {
+			File importFile = new File(getDataFolder() + File.separator
+					+ "import.xml");
+			new File(getDataFolder().toString()).mkdir();
+			this.logMessage("import.xml does not exist. Creating...");
+			try {
+				importFile.createNewFile();
+				this.logMessage("File created!");
+			} catch (IOException e) {
+				System.out.println("Cannot create File " + importFile.getPath()
+						+ File.separator + "import.xml");
+			}
 		}
 	}
 
 	private void createBookFile() {
-		File bookFile = new File(getDataFolder() + File.separator +  "books.txt");
-		this.logMessage("books.txt does not exist. Creating...");
-		
-		InputStream input = this.getResource("books.txt");
-		if (input != null) {
-			FileOutputStream output = null;
-			try {
-				output = new FileOutputStream(bookFile);
-				byte[] buf = new byte[8192];
-				int length = 0;
-				while ((length = input.read(buf)) > 0) {
-					output.write(buf, 0, length);
-				}
-				this.logMessage("books.txt created!");
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.out.println("Cannot create File " + bookFile.getPath()
-						+ File.separator + "books.txt");
-			} finally {
+		if (!checkBookFile()) {
+			File bookFile = new File(getDataFolder() + File.separator
+					+ "books.txt");
+			this.logMessage("books.txt does not exist. Creating...");
+
+			InputStream input = this.getResource("books.txt");
+			if (input != null) {
+				FileOutputStream output = null;
 				try {
-					input.close();
+					output = new FileOutputStream(bookFile);
+					byte[] buf = new byte[8192];
+					int length = 0;
+					while ((length = input.read(buf)) > 0) {
+						output.write(buf, 0, length);
+					}
+					this.logMessage("books.txt created!");
 				} catch (IOException e) {
-				}
-				try {
-					if (output != null)
-						output.close();
-				} catch (IOException e) {
+					e.printStackTrace();
+					System.out
+							.println("Cannot create File " + bookFile.getPath()
+									+ File.separator + "books.txt");
+				} finally {
+					try {
+						input.close();
+					} catch (IOException e) {
+					}
+					try {
+						if (output != null)
+							output.close();
+					} catch (IOException e) {
+					}
 				}
 			}
 		}
