cb5083489d289119d37aceb04a85e7770739c2b4
Uripura <uripura@gmx.de>
2012-08-22 00:07:37 +0200

    Added the Changes from TheE Reformated Source Made Startup less complicated

--- IOBookcase/src/de/minehattan/uripura/IOBookcase/IOBookcase.java
+++ IOBookcase/src/de/minehattan/uripura/IOBookcase/IOBookcase.java
@@ -1,100 +1,159 @@
 package de.minehattan.uripura.IOBookcase;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class IOBookcase extends JavaPlugin {
 
-	private Logger log = Logger.getLogger("minecraft");
-	private IOBookcaseBlockListener blockListener = new IOBookcaseBlockListener(this);
-	private IOBookcasePlayerListener playerListener = new IOBookcasePlayerListener(this);
-	public String pluginDirPath;
-	public File configFile;
-	public IOBookcaseConfig config;
+	private Logger log = Logger.getLogger( "minecraft");
+	private IOBookcaseBlockListener blockListener = new IOBookcaseBlockListener( this);
+	private IOBookcasePlayerListener playerListener = new IOBookcasePlayerListener( this);
+	private File pluginFolder;
+	private File configFile;
 
 	public void onEnable() {
 
 		PluginManager pm = this.getServer().getPluginManager();
-		/*
-		 * pm.registerEvent(Event.Type.PLAYER_INTERACT, playerListener,
-		 * Priority.Monitor, this); pm.registerEvent(Event.Type.BLOCK_BREAK,
-		 * blockListener, Priority.Highest, this);
-		 * pm.registerEvent(Event.Type.BLOCK_BURN, blockListener,
-		 * Priority.Monitor, this); pm.registerEvent(Event.Type.SIGN_CHANGE,
-		 * blockListener, Priority.Monitor, this);
-		 */
-		pm.registerEvents(this.blockListener, this);
-		pm.registerEvents(this.playerListener, this);
 
-		this.pluginDirPath = this.getDataFolder().getAbsolutePath();
-		this.configFile = new File(this.pluginDirPath + File.separator + "config.yml");
-		this.config = new IOBookcaseConfig(this.configFile);
+		pm.registerEvents( this.blockListener, this);
+		pm.registerEvents( this.playerListener, this);
 
-		if (!checkFile())
-			createFile();
-		if (!checkDatabase())
-			createDatabase();
+		pluginFolder = getDataFolder();
+		createConfig();
+		saveConfig();
 
-		this.logMessage("Enabeld");
+		createFile();
+		createDatabase();
+		createBookFile();
+
+		this.logMessage( "Enabeld");
 	}
 
 	public void onDisable() {
-		this.logMessage("Disabeld");
+		this.logMessage( "Disabeld");
 	}
 
-	public void logMessage(String msg) {
+	@Override
+	public boolean onCommand( CommandSender sender, Command command, String label, String[] args) {
+		if( command.getName().equalsIgnoreCase( "iobookcase")) {
+			if( sender instanceof Player && !( ( Player) sender).hasPermission( "iobookcase.reload"))
+				return true;
+
+			if( args.length > 0 && args[0].equalsIgnoreCase( "reload")) {
+				reloadConfig();
+				sender.sendMessage( ChatColor.YELLOW + "IOBookcase configuration file reloaded.");
+			} else
+				sender.sendMessage( ChatColor.YELLOW + "Use '/iobookcase reload' to reload the configuration file.");
+		}
+		return true;
+	}
+
+	public void logMessage( String msg) {
 		PluginDescriptionFile pdFile = this.getDescription();
-		this.log.info("[" + pdFile.getName() + " " + pdFile.getVersion() + "] " + msg);
+		this.log.info( "[" + pdFile.getName() + " " + pdFile.getVersion() + "] " + msg);
 	}
 
-	public void warnMessage(String msg) {
+	public void warnMessage( String msg) {
 		PluginDescriptionFile pdFile = this.getDescription();
-		this.log.warning("[" + pdFile.getName() + " " + pdFile.getVersion() + "] " + msg);
+		this.log.warning( "[" + pdFile.getName() + " " + pdFile.getVersion() + "] " + msg);
+	}
+	
+	public void errorMessage( String msg) {
+		PluginDescriptionFile pdFile = this.getDescription();
+		this.log.severe( "[" + pdFile.getName() + " " + pdFile.getVersion() + "] " + msg);
 	}
 
-	private void createDatabase() {
-		this.logMessage("bookcase.db does not exist. Creating...");
-		IOBookcaseDatabase database = new IOBookcaseDatabase();
-		if (database.createDatabase()) {
-			this.logMessage("Database created!");
-		} else
-			this.logMessage("Cannot create Database!");
-
-	}
-
-	private void createFile() {
-
-		File importFile = new File(getDataFolder() + File.separator + "import.xml");
-
-		new File(getDataFolder().toString()).mkdir();
-
-		this.logMessage("import.xml does not exist. Creating...");
-		try {
-			importFile.createNewFile();
-			this.logMessage("File created!");
-
-		} catch (IOException e) {
-			System.out.println("Cannot create File " + importFile.getPath() + File.separator + "import.xml");
+	private void createConfig() {
+		if( !pluginFolder.exists()) {
+			try {
+				pluginFolder.mkdir();
+				configFile.createNewFile();
+				
+				if( !configFile.exists()) {
+					getConfig().options().copyDefaults( true);
+					configFile = new File( pluginFolder, "config.yml");
+				}
+				
+			} catch( Exception e) {
+				this.errorMessage( "Cannot create Folder");
+				e.printStackTrace();
+			}
 		}
 	}
 
-	private boolean checkDatabase() {
-
-		File importcase = new File(getDataFolder() + File.separator + "bookcase.db");
-		if (importcase.exists())
-			return true;
-		else
-			return false;
+	private void createDatabase() {
+		if( !checkFile( "bookcase.db")) {
+			this.logMessage( "bookcase.db does not exist. Creating...");
+			IOBookcaseDatabase database = new IOBookcaseDatabase();
+			if( database.createDatabase()) {
+				this.logMessage( "Database created!");
+			} else
+				this.errorMessage( "Cannot create Database!");
+		}
 	}
 
-	private boolean checkFile() {
-		File importcase = new File(getDataFolder() + File.separator + "import.xml");
-		if (importcase.exists())
+	private void createFile() {
+		if( !checkFile( "import.xml")) {
+			File importFile = new File( getDataFolder() + File.separator + "import.xml");
+			new File( getDataFolder().toString()).mkdir();
+			this.logMessage( "import.xml does not exist. Creating...");
+			try {
+				importFile.createNewFile();
+				this.logMessage( "File created!");
+			} catch( IOException e) {
+				System.out.println( "Cannot create File " + importFile.getPath() + File.separator + "import.xml");
+			}
+		}
+	}
+
+	private void createBookFile() {
+		if( !checkFile( "books.txt")) {
+			File bookFile = new File( getDataFolder() + File.separator + "books.txt");
+			this.logMessage( "books.txt does not exist. Creating...");
+
+			InputStream input = this.getResource( "books.txt");
+			if( input != null) {
+				FileOutputStream output = null;
+				try {
+					output = new FileOutputStream( bookFile);
+					byte[] buf = new byte[8192];
+					int length = 0;
+					while( ( length = input.read( buf)) > 0) {
+						output.write( buf, 0, length);
+					}
+					this.logMessage( "books.txt created!");
+				} catch( IOException e) {
+					e.printStackTrace();
+					System.out.println( "Cannot create File " + bookFile.getPath() + File.separator + "books.txt");
+				} finally {
+					try {
+						input.close();
+					} catch( IOException e) {
+					}
+					try {
+						if( output != null)
+							output.close();
+					} catch( IOException e) {
+					}
+				}
+			}
+		}
+	}
+
+	private boolean checkFile( String file) {
+		File importcase = new File( getDataFolder() + File.separator + file);
+		if( importcase.exists())
 			return true;
 		else
 			return false;
--- IOBookcase/src/de/minehattan/uripura/IOBookcase/IOBookcasePlayerListener.java
+++ IOBookcase/src/de/minehattan/uripura/IOBookcase/IOBookcasePlayerListener.java
@@ -1,5 +1,10 @@
 package de.minehattan.uripura.IOBookcase;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Random;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -15,30 +20,78 @@ import de.minehattan.uripura.IOBookcase.IOBookcaseDatabase;
 public class IOBookcasePlayerListener implements Listener {
 
 	private IOBookcase plugin;
+	protected static Random rand = new Random();
 
-	public IOBookcasePlayerListener(IOBookcase instance) {
+	public IOBookcasePlayerListener( IOBookcase instance) {
 		this.plugin = instance;
 	}
 
+	protected String getBookLine() throws IOException {
+		RandomAccessFile file = new RandomAccessFile( new File( plugin.getDataFolder(), "books.txt"), "r");
 
-	@EventHandler(priority = EventPriority.MONITOR)
-	public void onPlayerInteract(PlayerInteractEvent event) {
+		long len = file.length();
+		byte[] data = new byte[500];
+
+		for( int tries = 0; tries < 3; tries++) {
+			int j = rand.nextInt( ( int) len);
+			if( tries == 2) { // File is too small
+				j = 0;
+			}
+			file.seek( j);
+			file.read( data);
+
+			StringBuilder buffer = new StringBuilder();
+			boolean found = j == 0;
+			byte last = 0;
+
+			for( int i = 0; i < data.length; i++) {
+				if( found) {
+					if( data[i] == 10 || data[i] == 13 || i >= len) {
+						if( last != 10 && last != 13) {
+							file.close();
+							return buffer.toString();
+						}
+					} else {
+						buffer.appendCodePoint( data[i]);
+					}
+				} else if( data[i] == 10 || data[i] == 13) { // Line feeds
+					found = true;
+				}
+
+				last = data[i];
+			}
+		}
+
+		file.close();
+		return null;
+	}
+
+	@EventHandler( priority = EventPriority.MONITOR)
+	public void onPlayerInteract( PlayerInteractEvent event) {
 		Player player = event.getPlayer();
 		String worldName = player.getWorld().getName();
 		Block block = event.getClickedBlock();
 
 		// Check if the player is actually interacting with a bookcase
 		// Also if the player holds a block we don't want to display text
-		if (event.isCancelled() || block.getType() != Material.BOOKSHELF)
+		if( event.isCancelled() || block.getType() != Material.BOOKSHELF || player.getItemInHand().getType() != Material.AIR)
 			return;
 
-		if (player.hasPermission("iobookcase.read") == false)
+		if( player.hasPermission( "iobookcase.read") == false)
 			return;
 
 		IOBookcaseDatabase connection = new IOBookcaseDatabase();
-		if (connection.checkCase(worldName, block.getX(), block.getY(), block.getZ())) {
-			connection.readCase(player, worldName, block.getX(), block.getY(), block.getZ());
+		if( connection.checkCase( worldName, block.getX(), block.getY(), block.getZ())) {
+			player.sendMessage( ChatColor.GRAY + plugin.getConfig().getString( "msg-pick-book"));
+			connection.readCase( player, worldName, block.getX(), block.getY(), block.getZ());
+		} else if( plugin.getConfig().getBoolean( "random-text")) {
+			player.sendMessage( plugin.getConfig().getString( "msg-pick-book"));
+			try {
+				player.sendMessage( ChatColor.YELLOW + this.getBookLine());
+			} catch( IOException e) {
+				player.sendMessage( ChatColor.RED + "Failed to fetch a line from the books file.");
+			}
 		} else
-			player.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("msg-empty-bookcase"));
+			player.sendMessage( ChatColor.YELLOW + plugin.getConfig().getString( "msg-empty-bookcase"));
 	}
 }
